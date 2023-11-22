813eec3ccb6dd747b1de5479be758145f810ec3a
neelvaria <neelvaria08@gmail.com>
2023-11-22 15:58:42 +0530

    initial

--- main/models.py
+++ main/models.py
@@ -18,6 +18,7 @@ class Skill(models.Model):
     def __str__(self):
         return self.name
 
+
 class UserProfile(models.Model):
     class Meta:
         verbose_name_plural = 'User Profiles'
@@ -32,3 +33,120 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+
+
+class ContactProfile(models.Model):
+    class Meta:
+        verbose_name_plural = 'Contact Profiles'
+        verbose_name = 'Contact Profile'
+        ordering = ["timestamp"]
+    
+    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(verbose_name="Name",max_length=100)
+    email = models.EmailField(verbose_name="Email")
+    message = models.TextField(verbose_name="Message")
+    
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Testimonial(models.Model):
+    class Meta:
+        verbose_name_plural = 'Testimonials'
+        verbose_name = 'Testimonial'
+        ordering = ["name"]
+    
+    thumbnail = models.ImageField(blank=True,null=True,upload_to="Testimonial")
+    name = models.CharField(max_length=200,blank=True,null=True)
+    role = models.CharField(max_length=200,blank=True,null=True)
+    quote = models.CharField(max_length=500,blank=True,null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class media(models.Model):
+    class Meta:
+        verbose_name_plural = 'Media Files'
+        verbose_name = 'Media'
+        ordering = ["name"]
+    
+    image = models.ImageField(blank=True,null=True,upload_to="media")
+    url = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    is_image = models.BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        if self.url:
+            self.is_image = False
+        super(media, self).save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name
+
+
+class Portfolio(models.Model):
+    class Meta:
+        verbose_name_plural = 'Portfolio Profiles'
+        verbose_name = 'Portfolio'
+        ordering = ["name"]
+    
+    date = models.DateTimeField(blank=True,null=True)
+    name = models.CharField(max_length=200,blank=True,null=True)
+    description = models.CharField(max_length=500,blank=True,null=True)
+    body = RichTextField(blank=True,null=True)
+    image = models.ImageField(blank=True,null=True,upload_to="portfolio")
+    slug = models.SlugField(null=True,blank=True)
+    
+    def save(self,*args,**kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Portfolio,self).save(*args,*kwargs)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f"/portfolio/{self.slug}"
+    
+
+class Blog(models.Model):
+    class Meta:
+        verbose_name_plural = 'Blog Profiles'
+        verbose_name = 'Blog'
+        ordering = ["timestamp"]
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
+    slug = models.SlugField(null=True, blank=True)
+    image = models.ImageField(blank=True, null=True, upload_to="blog")
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Blog, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+class Certificate(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Certificates'
+        verbose_name = 'Certificate'
+
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name    
