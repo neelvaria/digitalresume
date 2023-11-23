df3ea41c7b1a29042776c3718314aa27f60f502f
neelvaria <neelvaria08@gmail.com>
2023-11-23 09:19:51 +0530

    frontened

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    # path('',views.IndexView.as_views(),name="home"),
-    # path('contact/',views.ContactView.as_view(),name="contact"),
-    # path('portfolio',views.PortfolioView.as_view(),name="portfolio"),
-    # path('portfolio/<slug:slug>',views.PortfolioDetailView.as_view(),name="portfoliodetail"),
-    # path('blog/',views.BlogView.as_view(),name="blogs"),
-    # path('blog/<slug:slug>',views.BlogDetailView.as_view(),name="blog"),
+    path('', views.IndexView.as_view(), name="home"),
+    path('contact/',views.ContactView.as_view(),name="contact"),
+    path('portfolio',views.portfolioview.as_view(),name="portfolio"),
+    path('portfolio/<slug:slug>',views.portfoliodetialview.as_view(),name="portfoliodetail"),
+    path('blog/',views.Blogview.as_view(),name="blogs"),
+    path('blog/<slug:slug>',views.blogdetailview.as_view(),name="blog"),
 ]
